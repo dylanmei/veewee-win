1926e9fcfaa77fedad94b18cbf92830ddb9ebf92
dylanmei <dylanmei@gmail.com>
2013-03-25 23:33:54 -0700

    Initial commit

---
+++ definitions/windows-2008r2-pe2.7/install-vbox.bat
@@ -0,0 +1,3 @@
+cmd /c certutil -addstore -f "TrustedPublisher" a:oracle-cert.cer
+cmd /c e:\VBoxWindowsAdditions-amd64.exe /S
+cmd /c shutdown.exe /r /t 0 /d p:2:4 /c "Vagrant reboot for VBoxWindowsAdditions"
